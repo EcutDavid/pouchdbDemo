d46e168c6d88279de31378ed43814a010d7b936e
David Guan <DavidGuanDev@gmail.com>
2016-05-06 16:40:23 +0800

    Basic CR

--- manipulate.js
+++ manipulate.js
@@ -2,7 +2,27 @@ const PouchDB = require('pouchdb')
 
 const db = new PouchDB('http://localhost:5984/test')
 db.info().then(info => {
-  console.log(info);
+  console.log(info)
 })
 
-console.log('info');
+const originalDoc = {
+  '_id': 'book',
+  value: 20,
+}
+
+//May result conflit here, because document is already there after first execution
+db.put(originalDoc).then(() =>{
+  console.log('succeed')
+  db.get('book').then(d => {
+    console.log(d)
+  })
+}, (err) => {
+  console.log(err)
+})
+
+db.get('book').then(d => {
+  console.log(d)
+})
+
+
+console.log('info')
